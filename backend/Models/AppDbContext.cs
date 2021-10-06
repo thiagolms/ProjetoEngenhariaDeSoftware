6d8de97fa637774d3bc6573acb0b031f3e839960
Leandro Farias <leandro.sfarias997@gmail.com>
2021-10-06 16:38:55 -0300

    Entidade Usuário e AppDBcontext

---
+++ backend/Models/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace backend.Models
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Password=Ns17E!%Wz;Persist Security Info=True;User ID=lsifarias;Initial Catalog=CauaeDB;Data Source=servidordosqlserver.database.windows.net");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.UsuarioId);
+
+                entity.Property(e => e.Email).HasMaxLength(250);
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(80);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
