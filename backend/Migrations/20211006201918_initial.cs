f4a2d98b682a56713bcfad79bf5f61d221a3d736
Leandro Farias <leandro.sfarias997@gmail.com>
2021-10-06 17:25:37 -0300

    Migration

---
+++ backend/Migrations/20211006201918_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    Login = table.Column<string>(maxLength: 80, nullable: false),
+                    Senha = table.Column<string>(maxLength: 80, nullable: false),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Email = table.Column<string>(maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
