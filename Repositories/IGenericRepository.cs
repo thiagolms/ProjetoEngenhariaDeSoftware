35a3a14460e4b258c0eabf9bee2ad1786f195d30
Leandro Farias <leandro.sfarias997@gmail.com>
2021-10-07 23:44:35 -0300

    Remoção migration

---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Insert(T obj);
+        Task Update(int id, T obj);
+        Task Delete(int id);
+    }
+}
