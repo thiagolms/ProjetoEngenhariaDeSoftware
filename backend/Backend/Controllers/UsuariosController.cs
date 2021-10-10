b0d91916b3bf8a227c614aba592b3fae2af196f2
Leandro Farias <leandro.sfarias997@gmail.com>
2021-10-10 00:31:41 -0300

    Controlador de usuários

---
+++ backend/Backend/Controllers/UsuariosController.cs
@@ -0,0 +1,92 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        public UsuariosController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return " << Controlador UsuariosController :: WebApiUsuarios >> ";
+        }
+        [HttpPost("Criar")]
+        public async Task<ActionResult<UserToken>> CreateUser([FromBody] UserInfo model)
+        {
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return BuildToken(model);
+            }
+            else
+            {
+                return BadRequest("Usuário ou senha inválidos");
+            }
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] UserInfo userInfo)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password,
+                 isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "login inválido.");
+                return BadRequest(ModelState);
+            }
+        }
+        private UserToken BuildToken(UserInfo userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim("meuValor", "oque voce quiser"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            // tempo de expiração do token: 1 hora
+            var expiration = DateTime.UtcNow.AddHours(1);
+            JwtSecurityToken token = new JwtSecurityToken(
+               issuer: null,
+               audience: null,
+               claims: claims,
+               expires: expiration,
+               signingCredentials: creds);
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
--- backend/Backend/Models/UserInfo.cs
+++ backend/Backend/Models/UserInfo.cs
@@ -11,5 +11,6 @@ namespace Backend.Models
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Role { get; set; }
+        public string Password { get; internal set; }
     }
 }
