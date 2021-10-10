b73a5fe62e732b8ee5107796b962b6f11b3e900d
Leandro Farias <leandro.sfarias997@gmail.com>
2021-10-10 00:22:03 -0300

    Contexto

--- backend/Backend/Models/ApplicationUser.cs
+++ backend/Backend/Models/ApplicationUser.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Backend.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
     }
 }
