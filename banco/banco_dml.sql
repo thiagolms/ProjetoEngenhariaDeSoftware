602e2182fd575f8f3e6756a061c04257a273a315
Leandro Farias <leandro.sfarias997@gmail.com>
2021-11-06 16:18:53 -0300

    Script SQL dml

--- banco/banco_dml.sql
+++ banco/banco_dml.sql
@@ -4,3 +4,6 @@ insert into roles (nome) values ('PESQUISADOR');
 
 insert into usuarios (nome, email, senha, cpf, rg, id_role)
 values ('Administrador', 'administrador@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '00000000000', '000000000', 5);
+
+insert into usuarios (nome, email, senha, cpf, rg, id_role)
+values ('Secretaria', 'secretaria@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '00000000000', '000000000', 15);
